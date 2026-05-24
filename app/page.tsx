497950a726f636c5346364938b0ed31da22d8e8b
theoheron <theron@ucsd.edu>
2026-05-24 12:34:18 -0700

    Update website content and figures

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
 
             <div className="mt-7 text-center">
               <p className="mt-4 text-lg leading-7">
-                Incoming M.S. Chemical Engineering Student,{" "}
+                M.S. Chemical Engineering Student,{" "}
                 <span className="font-normal" style={{ color: "#5B8FB9" }}>
                   Columbia University
                 </span>
